177d4758aaa6f949f7c9573d49f39f436cfbe4fc
Manuel Camacho Padilla <manu320camacho@gmail.com>
2022-12-10 19:41:53 -0600

    lesson 5

--- 04-Webpack/src/script.js
+++ 04-Webpack/src/script.js
@@ -1,1 +1,32 @@
 import './style.css'
+import * as THREE from 'three'
+
+// Scene
+const scene = new THREE.Scene();
+
+// Objects
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+// Sizes
+const sizes = {
+    width: 800,
+    height: 600
+}
+
+// Camera
+// fov = field of view
+// PerspectiveCamera(fov, aspect ratio)
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.set(2,1,4)
+scene.add(camera);
+
+// Renderer
+const canvas = document.querySelector('.webgl');
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.render(scene, camera);
